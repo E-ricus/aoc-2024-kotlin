e2f7d3014573734ce16166ab8ccc2969af85a312
Eric Puentes <eric.david2333@gmail.com>
2024-12-07 08:36:08 +0100

    seventh day

---
+++ src/Day07.kt
@@ -0,0 +1,64 @@
+fun main() {
+    fun evaluate(nums: List<Long>, index: Int, currentValue: Long, target: Long, useCombinator: Boolean): Boolean {
+        if (index == nums.lastIndex) {
+            return currentValue == target
+        }
+
+        val nextIndex = index + 1
+        val nextValue = nums[nextIndex]
+        when (useCombinator) {
+            true -> {
+                val concatenatedValue = (currentValue.toString() + nextValue.toString()).toLong()
+
+                return evaluate(nums, nextIndex, currentValue + nextValue, target, true) ||
+                        evaluate(nums, nextIndex, currentValue * nextValue, target, true) ||
+                        evaluate(nums, nextIndex, concatenatedValue, target, true)
+
+            }
+
+            false -> {
+
+                return evaluate(nums, nextIndex, currentValue + nextValue, target, false) ||
+                        evaluate(nums, nextIndex, currentValue * nextValue, target, false)
+
+            }
+        }
+    }
+
+    fun isTarget(nums: List<Long>, target: Long, useCombinator: Boolean): Boolean {
+        if (nums.isEmpty()) return false
+        return evaluate(nums, 0, nums.first(), target, useCombinator)
+    }
+
+    fun part1(input: List<String>): Long {
+        val equations = input.map {
+            val parts = it.split(":")
+            val numbers = parts[1].trim().split(" ").map { it.toLong() }
+            parts[0].toLong() to numbers
+        }
+        return equations.sumOf { (target, numbers) ->
+            if (isTarget(numbers, target, false)) target else 0
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        val equations = input.map {
+            val parts = it.split(":")
+            val numbers = parts[1].trim().split(" ").map { it.toLong() }
+            parts[0].toLong() to numbers
+        }
+        return equations.sumOf { (target, numbers) ->
+            if (isTarget(numbers, target, true)) target else 0
+        }
+    }
+
+    val testInput = readInputByLines("test/Day07")
+    part1(testInput).println()
+    check(part1(testInput) == 3749L)
+    part2(testInput).println()
+    check(part2(testInput) == 11387L)
+
+    val input = readInputByLines("input/Day07")
+    part1(input).printResult("Part 1")
+    part2(input).printResult("Part 2")
+}
